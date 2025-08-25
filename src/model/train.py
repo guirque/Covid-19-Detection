03e19f9c2b95fa53a2a7628bf5c3dc6d0330ec3f
guirque <guirque@gmail.com>
2025-08-25 20:08:12 -0300

    feat: updating train loop
    Added accuracy calculation during training and data loding on every epoch (shuffling data)

--- src/model/train.py
+++ src/model/train.py
@@ -1,16 +1,32 @@
 from tqdm import tqdm
+import torch
+from torch.utils.data import DataLoader
+from setup import BATCH_SIZE
 
+def load_dataset(dataset):
+    loader = DataLoader(
+        dataset=dataset,
+        batch_size=BATCH_SIZE,
+        shuffle=True,
+        num_workers=3
+    )
 
-def train(model, num_epochs, num_batches, device, optimizer, scheduler, dataloader, loss):
+    return iter(loader)
+
+def train(model, num_epochs, num_batches, device, optimizer, scheduler, dataset, loss, num_imgs):
     # Loop
     for epoch in range(num_epochs):
-        iterable_ds = iter(dataloader) # reinitialize batches whenever we run a new epoch
+        accuracy = 0
+
+        #iterable_ds = iter(dataloader) # reinitialize batches whenever we run a new epoch
+        iterable_ds = load_dataset(dataset)
+
         for i in tqdm(range(num_batches), desc=f"Epoch {epoch}"):
             try:
                 batch, labels = next(iterable_ds) # we get the images (batch) and corresponding labels. That's returned by __getitem__, it seems.
                 batch = batch.to(device=device)
                 labels = labels.to(device=device)
-                #print(model.cl1.weight.device, batch.device)
+
                 final_outputs = model(batch)
 
                 loss_results = loss(final_outputs, labels)
@@ -20,5 +36,13 @@ def train(model, num_epochs, num_batches, device, optimizer, scheduler, dataload
                 optimizer.step()
 
                 scheduler.step()
+
+                # Evaluating
+                pred = torch.softmax(final_outputs, dim=0) 
+                pred_classes = torch.max(pred, dim=1).indices
+                right_answers = (pred_classes == labels).sum()
+                accuracy += right_answers
+
             except StopIteration:
                 print(f'Exception Encountered: batch {i}/{num_batches} in epoch {epoch}')
+        print(f"Epoch {epoch} | Accuracy: {(accuracy*100/num_imgs):.4f}%")
