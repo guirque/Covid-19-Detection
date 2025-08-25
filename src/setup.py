c226343c1dddeef09ce9c6f3bd8ebad16f1057e8
guirque <guirque@gmail.com>
2025-08-25 20:12:36 -0300

    feat: updating setup constants

--- src/setup.py
+++ src/setup.py
@@ -2,16 +2,17 @@ import sys
 import os
 
 # SRC_CONSTANTS
-BATCH_SIZE = 10
-NUM_EPOCHS = 50
-IMAGE_WIDTH = 650
-IMAGE_HEIGHT = 650
+BATCH_SIZE = 14
+NUM_EPOCHS = 25
+IMAGE_WIDTH =  1118 # 650 1118
+IMAGE_HEIGHT =  1440 # 650 1440
 LEARNING_RATE=0.01
 
 # PATH CONSTANTS
 SRC_PATH = os.path.dirname(__file__)
 PROJECT_PATH = os.path.dirname(SRC_PATH)
 DATA_PATH = os.path.join(PROJECT_PATH, 'dataset', 'Covid19-dataset')
+SAVE_PATH = os.path.join(SRC_PATH, 'model', 'saved')
 
 if SRC_PATH not in sys.path:
     sys.path.append(SRC_PATH)
