7ccb1237e67bc27115c37db24b1730064b90f31a
guirque <guirque@gmail.com>
2025-08-29 16:26:13 -0300

    feat: adding GPU support to evaluation_single
    Also including tqdm for function to show loading bar

--- src/data/evaluation.py
+++ src/data/evaluation.py
@@ -45,8 +45,7 @@ def evaluate_model(model: torch.nn.Module, test_data, batch_size):
 
     print(confusion_matrix(labels_all, predicted_classes, labels=[0, 1, 2]))
 
-def evaluate_single(model:torch.nn.Module, classes_list:list):
-    # Checking Biggest Image Sizes
+def evaluate_single(model:torch.nn.Module, classes_list:list, device):
     from glob import glob
     from setup import DATA_PATH, IMAGE_HEIGHT, IMAGE_WIDTH
     import os
@@ -55,32 +54,30 @@ def evaluate_single(model:torch.nn.Module, classes_list:list):
     from torchvision import transforms
 
     model = model.eval()
+    model.to(device)
 
     class_num = 0
     accuracy = 0
     num_elements = 0
     for class_str in classes_list:
         imgs = glob(os.path.join(DATA_PATH, 'test', class_str, '*.jpg'), recursive=True) + glob(os.path.join(DATA_PATH, 'test', class_str, '*.png'), recursive=True) + glob(os.path.join(DATA_PATH, 'test', class_str, '*.jpeg'), recursive=True) 
-        print('-------------------- Running Images of Class ', class_str)
-        #print(imgs)
 
-        for img in imgs:
+        for img in tqdm(imgs, desc=f'Testing Against Images of Class {class_str}'):
             num_elements += 1
             pil_image = Image.open(img)
             pil_image = pil_image.convert('RGB')
-            tensor = pil_to_tensor(pil_image)
 
             transform = transforms.Compose([transforms.Resize(size=(IMAGE_WIDTH, IMAGE_HEIGHT)), transforms.ToTensor(), transforms.Normalize([0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])])
             model_input = transform(pil_image).float().unsqueeze(0)
+
+            model_input = model_input.to(device=device)
+
             result = model(model_input)
 
             probabilities = torch.softmax(result, dim=1)
             predicted_class = torch.argmax(probabilities).item()
             if predicted_class == class_num:
                 accuracy += 1
-            #print('Class predicted: ', predicted_class, '| Real class: ', class_num)
-
-            #print(f'Probabilities: {probabilities} \n\n')
         
         class_num += 1
     print(f'\nAccuracy: {accuracy}/{num_elements} | {(accuracy*100/num_elements):.4f}%')
