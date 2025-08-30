d2df8935cee9bdfa56848739bf10e95340e34f21
guirque <guirque@gmail.com>
2025-08-29 21:52:25 -0300

    fix: replacing deprecated parameter and adding reference

--- src/model/load_model.py
+++ src/model/load_model.py
@@ -1,21 +1,19 @@
 from torch import nn, softmax, relu, load
 from model.CNN_Model import CNN_Model
 from setup import IMAGE_HEIGHT, IMAGE_WIDTH, FINE_TUNE
-from torchvision.models import resnet18
+from torchvision.models import resnet18, ResNet18_Weights
 
 def create_model(fine_tune=False):
 
     model = None
 
     if fine_tune:
-        print(f'Using fine tuning strategy.')
         # Based on: https://youtu.be/c36lUUr864M?si=nLzRzN_VFT8HSmiu
-        model = resnet18(pretrained=True)
+        model = resnet18(weights=ResNet18_Weights.DEFAULT)
         for params in model.parameters():
             params.requires_grad = False
         model.fc = nn.Linear(in_features=model.fc.in_features, out_features=3) # adjusting last layer to fit our 3 classes
     else:
-        print(f'Using custom model.')
         model = CNN_Model(image_size=(IMAGE_WIDTH, IMAGE_HEIGHT))
 
     return model
